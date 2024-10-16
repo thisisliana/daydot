2a81af64c89f92143ee3b93105465d6290135edc
thisisliana <lianaquinlivan@gmail.com>
2024-10-16 16:06:08 +1000

    Update ContactBlock.tsx with new address

--- src/components/ContactBlock.tsx
+++ src/components/ContactBlock.tsx
@@ -14,8 +14,8 @@ const ContactBlock = () => {
       <BodyText text={'You can reach me at: +61 (0) 422 942 625'} />
 
       <Typography fontWeight={'bold'} sx={{ mt: 2 }}>Location</Typography>
-      <BodyText text={'6 broadway street, Woolloongabba, Brisbane, QLD'} />
-      <BodyText text={'(Free parking onsite)'} />
+      <BodyText text={'16 Barrack St, Cannon Hill, Brisbane, QLD'} />
+      <BodyText text={'(Enter through the door on the left- no need to ring the bell)'} />
       <LocationMap height={400} width={400} />
     </>
   )
